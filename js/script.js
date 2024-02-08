a595d5ed8ac4ac34a855d783c50904c1d03981f1
Emilio Zapata <124ezapata@frhsd.com>
2024-02-08 11:40:09 -0500

    finish about

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var x = document.getElementById("about");
 x.addEventListener("mouseover", myFunction);
 x.addEventListener("mouseout", mySecFunction);
 
-function myFunction() {
+/* function myFunction() {
   document.getElementById("").innerHTML += ;
-}
+}*/
 
 function mOut(obj) {
     document.getElementById('about').style.removeBackgroundImage =  "url('img/mindfulness')";
